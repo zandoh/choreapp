2b6c591084028e77b912343a0533aa74f7d3772f
Zac Clark <zandoh9@gmail.com>
2019-12-02 14:45:23 -0500

    WIP: Remove unneeded cognito mock

--- client/src/services/__mocks__/cognito.ts
+++ client/src/services/__mocks__/cognito.ts
@@ -1,8 +1,5 @@
 import { mockUser } from "../../testUtil";
 
-const generatedMock = jest.genMockFromModule("react-redux");
-
 export const CognitoService = {
-  ...generatedMock,
   getUserTokenFromLocalStorage: () => mockUser.jwt
 };
