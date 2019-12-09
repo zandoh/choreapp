36299f02f8afa88e142a6cb51e47d0ad1d7be08f
Zachary Clark <>
2019-12-09 13:01:50 -0500

    WIP: apollo federation

--- api/src/services/chores/src/resolvers/getAuthor.ts
+++
@@ -1,7 +0,0 @@
-import { Post, User } from 'schemaTypes';
-
-const getAuthor = (post: Post, args: {}, context: AppGraphQLContext): User => {
-  return { id: post.author.id };
-};
-
-export default getAuthor;
