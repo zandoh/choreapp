7516cd92d28523beff9a44506ae8d15bbec42771
Zachary Clark <>
2020-02-21 14:18:43 -0500

    Updates col sorting

--- client/src/components/Board/Board.tsx
+++ client/src/components/Board/Board.tsx
@@ -5,6 +5,22 @@ import { Container } from "./styled";
 import { data } from "./data";
 import { IChore, ChoreState } from "../../types/board";
 
+// moves a chore in an array
+const moveChore = (arr: IChore[], chore: IChore, newIndex: number) => {
+	console.log('chore ', chore);
+	if (newIndex >= arr.length) {
+		newIndex = arr.length - 1;
+	}
+	const resArr = arr.filter((ch) => ch.id !== chore.id); // remove changed chore
+	resArr.sort((a, b) => a.index - b.index); // sort on index
+	resArr.splice(newIndex, 0, chore); // add item at index
+
+	for (let i = 0; i < resArr.length; i++) {
+		resArr[i].index = i;
+	}
+	return resArr;
+};
+
 const Board: React.FC = () => {
 	const [chores, setChores] = useState<IChore[]>(data);
 
@@ -22,23 +38,34 @@ const Board: React.FC = () => {
 			return;
 		}
 
-		// if the draggable switched droppables || switched positions
-		if (
-			result.source.droppableId !== result.destination.droppableId ||
-			result.source.index !== result.destination.index
-		) {
+		const changedCol = result.source.droppableId !== result.destination.droppableId;
+		const changedIndex = result.source.index !== result.destination.index;
+		let tempChores = chores.slice();
+		
+		// if the draggable switched droppables
+		if (changedCol || changedIndex) {
 			// will want to make api call here
-			setChores(
-				chores.map(chore => {
+			if (changedCol) {
+				tempChores = chores.map(chore => {
 					if (chore.id !== result.draggableId) return chore;
 					return {
 						...chore,
-						state: ChoreState[result.destination!.droppableId],
-						index: result.destination!.index
+						state: ChoreState[result.destination!.droppableId]
 					};
-				})
-			);
+				});
+			}
+			
+			// if the draggable switched indexes 
+			if (changedIndex) {
+				const colChores = tempChores.filter((chore) => chore.state === ChoreState[result.destination!.droppableId])
+				const chore = tempChores.find((chore) => chore.id === result.draggableId)!;
+				const destIndex = result.destination.index;
+				tempChores.concat(moveChore(colChores, chore, destIndex));
+			}
 		}
+
+		// will want to make api call here
+		setChores(tempChores);
 	};
 
 	return (
